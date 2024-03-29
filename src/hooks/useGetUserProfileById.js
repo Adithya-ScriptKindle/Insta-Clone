47fba887473ca026f043e75c9d810960a924c692
Adithya-ScriptKindle <adisursad@gmail.com>
2024-03-29 12:43:48 -0400

    all changes

--- src/hooks/useGetUserProfileById.js
+++ src/hooks/useGetUserProfileById.js
@@ -1,34 +1,35 @@
 import { useEffect, useState } from "react";
-import {useShowToast} from "./useShowToast";
+import { useShowToast } from "./useShowToast";
 import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 
 const useGetUserProfileById = (userId) => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [userProfile, setUserProfile] = useState(null);
-	const showToast = useShowToast();
+  const [isLoading, setIsLoading] = useState(true);
+  const [userProfile, setUserProfile] = useState(null);
+  const showToast = useShowToast();
 
-	useEffect(() => {
-		const getUserProfile = async () => {
-			setIsLoading(true);
-			setUserProfile(null);
-			try {
-				const userRef = await getDoc(doc(firestore, "users", userId));
-				console.log(userRef.exists());
-				if (userRef.exists()) {
-					setUserProfile(userRef.data());
-				}
-			} catch (error) {
-				showToast("Error", error.message, "error");
-			} finally {
-				setIsLoading(false);
-			}
-		};
-		getUserProfile();
-	}, [showToast, setUserProfile, userId]);
-	
-	return { isLoading, userProfile, setUserProfile };
-	
+  useEffect(() => {
+    const getUserProfile = async () => {
+      setIsLoading(true);
+      setUserProfile(null);
+      try {
+        const userRef = await getDoc(doc(firestore, "users", userId));
+        console.log(userRef);
+        console.log(userRef.exists());
+        console.log(userRef.data());
+        if (userRef.exists()) {
+          setUserProfile(userRef.data());
+        }
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUserProfile();
+  }, [showToast, setUserProfile, userId]);
+
+  return { isLoading, userProfile, setUserProfile };
 };
 
 export default useGetUserProfileById;
